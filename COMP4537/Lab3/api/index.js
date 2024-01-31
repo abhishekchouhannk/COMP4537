8b5ddc68bf620fbab3d9870563e3cbd962ba43e3
Abhishek <abhishekchouhannk@gmail.com>
2024-01-31 11:51:47 -0800

    added vercel.json to configre hosting

--- COMP4537/Lab3/api/index.js
+++ COMP4537/Lab3/api/index.js
@@ -1,20 +1,24 @@
-const http = require('http');
+// const http = require('http');
 
-function route(req, res) {
-  if (req.url === '/') {
-    res.wrtieHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hello, this is Abhishek's Node api.");
-  }
-  else {
-    res.writeHead(400, {'Content-Type': 'text/plain'});
-    res.end('404 - Not Found');
-  }
+// function route(req, res) {
+//   if (req.url === '/') {
+//     res.wrtieHead(200, {'Content-Type': 'text/plain'});
+//     res.end("Hello, this is Abhishek's Node api.");
+//   }
+//   else {
+//     res.writeHead(400, {'Content-Type': 'text/plain'});
+//     res.end('404 - Not Found');
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// const PORT = process.env.PORT || 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// })
+
+module.exports = (req, res) => {
+  res.status(200).send("<h1>Hello world</h1>");
 }
-
-const server = http.createServer(handleRequest);
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
